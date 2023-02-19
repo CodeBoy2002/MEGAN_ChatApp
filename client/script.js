518786b3b2888e1036f60ebb50cb39bac27104a5
Sumit <marvelsumit2002@gmail.com>
2023-02-19 19:12:31 +0530

    fifth commit

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://meganchat.onrender.com', {
+    const response = await fetch(`https://meganchat.onrender.com`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
