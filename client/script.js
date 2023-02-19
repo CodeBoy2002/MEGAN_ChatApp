cce91097c3a31f6b1440169b71ddec4aa87a4339
Sumit <marvelsumit2002@gmail.com>
2023-02-19 18:53:56 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch the data from the server --> Bot's response
-  const response = await fetch('http://localhost:8000', {
+  const response = await fetch('https://meganchat.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
